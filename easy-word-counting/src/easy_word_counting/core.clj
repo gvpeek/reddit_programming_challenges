9e56ec8927994eac048ce726bcc8814ce31f7643
George Peek <peekgv@its-app-mai133.its.utexas.edu>
2014-12-11 11:16:12 -0600

    More revisions to finally get it working at a basic level

--- easy-word-counting/src/easy_word_counting/core.clj
+++ easy-word-counting/src/easy_word_counting/core.clj
@@ -3,43 +3,27 @@
   (require [clojure.string :as str])
   (require [clojure.java.io :as io]))
 
-;;(defn -main
-;;  "Reads in source book text and counts the number of occurrences of each word in the text."
-;;  [& args]
-;;  ;; set file path, concatted strings return a vector of characters, so converting to string
-;;  (def file-path (apply str (concat (System/getProperty "user.dir") "/resources/pg47498.txt")))
-;;  (def text-lines (with-open [book-text (io/reader file-path)] (line-seq book-text)))
-;;  (defn get-line-words [line] (str/split line #" "))
-;;  (defn strip-punc [word] (apply str (remove #((set '(\. \; \: \, \( \) \' \+ \- \|)) %) word)))
-;;  (defn count-word [word word-map] (update-in word-map [word] (fnil inc 0)))
-;;  (def word-counts {})
-;;  (def words (map get-line-words text-lines))
-;;  (defn dict-inc [m word] (update-in m [word] (fnil inc 0)))
-  
-;;  (println text-lines)
-;;   (reduce dict-inc word-counts words)
-;; )
-	      ;; skip spaces, otherwise catalog word
-	      ;;  (if (= word "") nil (strip-punc word) )
-	      ;;)
-;;	    )
-;;  (println (take 10 words))
-
 (defn -main
   "Reads in source book text and counts the number of occurrences of each word in the text."
   [& args]
   
-  (defn strip-punc [word] (apply str (remove #((set '(\. \; \: \, \( \) \' \+ \- \|)) %) word)))
-  (defn get-line-words [line] (re-seq #"\w+" (str/trim line)))
-  (defn dict-inc [m word] (update-in m [word] (fnil inc 0)))
-  
   (def file-path (apply str (concat (System/getProperty "user.dir") "/resources/pg47498.txt")))
+  ;; line-seq creates a lazy sequence, so we need to add a doall to realize it
   (def text-lines (with-open [book-text (io/reader file-path)] (doall (line-seq book-text))))
 
+  ;; remove produces a vector of characters, so need to turn them back into strings
+  (defn strip-punc [word] (apply str (remove #((set '(\. \; \: \, \( \) \' \+ \- \| \_)) %) word)))
+  (defn get-line-words [word-list line] (into word-list (re-seq #"\w+" (str/trim line))))
+  ;; lifted dict-inc from https://blog.safaribooksonline.com/2013/08/14/clojure-thinking-in-hashmaps/ 
+  ;; didn't intend to use someone elses solution, but after stubling upon an elegant one
+  ;; it was hard to put it out of my mind to do it diffently (and likely worse)
+  ;; From the page... fnil is a function, which creates a function that will use a default value when passed nil
+  (defn dict-inc [word-counts word] (update-in word-counts [word] (fnil inc 0)))
+  
   (println
     (reduce dict-inc {}
       (map strip-punc 
-        (map get-line-words ["This is one sentence." "This is another snetence."])
+        (reduce get-line-words [] text-lines)
       )
     )
   )
