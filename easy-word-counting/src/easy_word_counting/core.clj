2b8eefb143fac7246190f40f3956e05f43b75c61
gvpeek <gvpeek@gmail.com>
2014-12-11 00:02:37 -0600

    Major revision on logic. Still not producing the right result. counting each line instead of each word.

--- easy-word-counting/src/easy_word_counting/core.clj
+++ easy-word-counting/src/easy_word_counting/core.clj
@@ -3,20 +3,43 @@
   (require [clojure.string :as str])
   (require [clojure.java.io :as io]))
 
+;;(defn -main
+;;  "Reads in source book text and counts the number of occurrences of each word in the text."
+;;  [& args]
+;;  ;; set file path, concatted strings return a vector of characters, so converting to string
+;;  (def file-path (apply str (concat (System/getProperty "user.dir") "/resources/pg47498.txt")))
+;;  (def text-lines (with-open [book-text (io/reader file-path)] (line-seq book-text)))
+;;  (defn get-line-words [line] (str/split line #" "))
+;;  (defn strip-punc [word] (apply str (remove #((set '(\. \; \: \, \( \) \' \+ \- \|)) %) word)))
+;;  (defn count-word [word word-map] (update-in word-map [word] (fnil inc 0)))
+;;  (def word-counts {})
+;;  (def words (map get-line-words text-lines))
+;;  (defn dict-inc [m word] (update-in m [word] (fnil inc 0)))
+  
+;;  (println text-lines)
+;;   (reduce dict-inc word-counts words)
+;; )
+	      ;; skip spaces, otherwise catalog word
+	      ;;  (if (= word "") nil (strip-punc word) )
+	      ;;)
+;;	    )
+;;  (println (take 10 words))
+
 (defn -main
   "Reads in source book text and counts the number of occurrences of each word in the text."
   [& args]
-  ;; set file path, concatted strings return a vector of characters, so converting to string
+  
+  (defn strip-punc [word] (apply str (remove #((set '(\. \; \: \, \( \) \' \+ \- \|)) %) word)))
+  (defn get-line-words [line] (re-seq #"\w+" (str/trim line)))
+  (defn dict-inc [m word] (update-in m [word] (fnil inc 0)))
+  
   (def file-path (apply str (concat (System/getProperty "user.dir") "/resources/pg47498.txt")))
-  (defn strip-punc [word] (str (remove #((set (\. \; \: \,)) %) word)))
-  ;; open file
-  (with-open [book-text (io/reader file-path)]
-  ;; read each line
-    (doseq [line (line-seq book-text)]
-      ;; split the line on spaces
-      (doseq [word (str/split line #" ")]
-      ;; skip spaces, otherwise catalog word
-        (if (= word "") nil (println #((strip-punc word))))
+  (def text-lines (with-open [book-text (io/reader file-path)] (doall (line-seq book-text))))
+
+  (println
+    (reduce dict-inc {}
+      (map strip-punc 
+        (map get-line-words ["This is one sentence." "This is another snetence."])
       )
     )
   )
