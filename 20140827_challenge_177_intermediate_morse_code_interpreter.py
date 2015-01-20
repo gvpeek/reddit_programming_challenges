1a7e05832e7442dbc4563ce3baec33a9475bde3a
gvpeek <gvpeek@gmail.com>
2015-01-19 23:40:01 -0600

    Added methods for tone and pause durations. Improved timings.

--- 20140827_challenge_177_intermediate_morse_code_interpreter.py
+++ 20140827_challenge_177_intermediate_morse_code_interpreter.py
@@ -4,6 +4,31 @@ from struct import pack
 from math import sin
 
 RATE=44100
+base_duration=.1
+
+def long_tone():
+    tone = ''
+    for i in xrange(0, int(RATE * (base_duration * 3))):
+        tone += pack('h', max_vol * sin(i * 4000.0 / RATE))
+    return tone
+        
+def short_tone():
+    tone = ''
+    for i in xrange(0, int(RATE * base_duration)):
+        tone += pack('h', max_vol * sin(i * 4000.0 / RATE)) 
+    return tone
+    
+def letter_space():
+    tone = ''
+    for i in xrange(0, int(RATE * (base_duration * 3))):
+        tone += pack('h', 0) 
+    return tone
+
+def word_space():
+    tone = ''
+    for i in xrange(0, int(RATE * (base_duration * 7))):
+        tone += pack('h', 0) 
+    return tone
 
 wave_file = wave.open('test_mono.wav', 'w')
 ## params = nchannels, sampwidth, framerate, nframes, comptype, compname
@@ -11,9 +36,12 @@ wave_file.setparams((1, 2, RATE, 0, 'NONE', 'not compressed'))
 max_vol=2**15-1.0 #maximum amplitude
 wave_data = ''
 
-for i in xrange(0, RATE*3):
-    print sin(i * 500.0 / RATE)
-    wave_data += pack('h', max_vol * sin(i * 500.0 / RATE)) #500Hz
+wave_data += short_tone()
+wave_data += letter_space()
+wave_data += long_tone()
+wave_data += word_space()
+wave_data += short_tone()
+
 
 wave_file.writeframes(wave_data)
 wave_file.close()
