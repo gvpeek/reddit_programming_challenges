1b2c5b5c3da5197f0688170819eb763b604ac250
George Peek <gvpeek@yahoo.com>
2015-01-27 06:24:35 -0600

    Added basic user input

--- 20150107_challenge_196_intermediate_rail_fence.py
+++ 20150107_challenge_196_intermediate_rail_fence.py
@@ -1,21 +1,34 @@
 test = {'THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG' : 'TCNMRZHIKWFUPETAYEUBOOJSVHLDGQRXOEO'}
 
-current_level = 0
-direction = 1
-depth = 4
-rail_fences =[[] for x in xrange(depth)]
-cipher = ''
+def encode(depth, message):
+	current_level = 0
+	direction = 1
+	rows =[[] for x in xrange(depth)]
+	cipher = ''
 
-for message, result in test.iteritems():
 	for char in message:
-		rail_fences[current_level].append(char)
+		rows[current_level].append(char)
 		current_level += (1 * direction)
 		if current_level == 0:
 			direction = 1
 		elif current_level == depth - 1:
 			direction = -1
-	for fence in rail_fences:
-		for char in fence:
+	for row in rows:
+		for char in row:
 			cipher += char
 	print cipher
-	print 'Test passed!' if cipher == result else 'Test Failed!'
+	
+
+if __name__ == '__main__':
+    while True:
+    	user_input = raw_input('Enter a task ("enc" or "dec"), depth and message: ')
+    	try:
+	    	task, depth, message = user_input.split()
+	    	if task not in ['enc', 'dec']:
+	    		print 'Please enter a valid task, "enc" or "dec"'
+	    	if not depth.isdigit():
+	    		print 'Depth must be an integer.'
+	    	depth = int(depth)
+	    	encode(depth, message)
+    	except ValueError, e:
+    		print 'Please enter the correct number of arguments.'
