b99fa0d9529b58c9c494a703d0a2f66197a22c5c
gvpeek <gvpeek@gmail.com>
2015-01-29 06:32:11 -0600

    added total word and scene word counting, plus identifying top three scene words

--- 20140829_challenge_177_hard_script_it_language.py
+++ 20140829_challenge_177_hard_script_it_language.py
@@ -1,11 +1,41 @@
 import os
-from bs4 import BeautifulSoup
+import re
+import heapq
+
+from collections import defaultdict
 
 execution_dir = os.path.dirname(os.path.realpath(__file__))
-file_path = os.path.join(execution_dir, 'mphg.htm')
+file_path = os.path.join(execution_dir, 'holy_grail.txt')
 
+stage_directions = 0
+scene = 0
+total_words = 0
+words_per_scene = 0
+scene_words = defaultdict(int)
+with open(file_path, 'r+') as script:
+    for line in script:
+        print line
+        scene_nbr = re.match('^Scene (\d+)', line)
+        if scene_nbr:
+            if scene:
+                print
+                print 'Scene', scene, words_per_scene
+                word_list = []
+                for word, count in scene_words.iteritems():
+                    heapq.heappush(word_list, (count, word))
+                for count, top_word in heapq.nlargest(3, word_list):
+                    pct = count / float(words_per_scene)
+                    print 'Word: {0} \t Count: {1} \t Pct: {2:.2%}'.format(top_word, count, pct)
+            words_per_scene = 0
+            scene = scene_nbr.group(1)
+        if '[' in line and ']' in line:
+            stage_directions += 1
+        else:
+            words = line.split()
+            total_words += len(words)
+            words_per_scene += len(words)
+            for word in words:
+                scene_words[word] += 1
+            
 
-soup = BeautifulSoup(open(file_path))
-
-# print soup.get_text()
-print soup.prettify()
+    print stage_directions
